1d86b747214353313b2155c13a101ba381603f67
woong99 <wg.jeong@springworks.co.kr>
2024-06-02 13:23:14 +0900

    test

--- src/test/java/potatowoong/potatomallback/PotatoMallBackApplicationTests.java
+++ src/test/java/potatowoong/potatomallback/PotatoMallBackApplicationTests.java
@@ -1,5 +1,6 @@
 package potatowoong.potatomallback;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PotatoMallBackApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
